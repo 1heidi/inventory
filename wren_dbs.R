5c622877712eac3b026e6964a591551901691fb0
Heidi J. Imker <oneheidihere@gmail.com>
2021-06-11 15:42:45 -0500

    updating

--- wren_dbs.R
+++ wren_dbs.R
@@ -1,30 +1,30 @@
-# Starting with Wren's data from: 
-# Wren, Jonathan D et al. 
-# “Use it or lose it: citations predict the continued online availability of 
-# published bioinformatics resources.” 
-# Nucleic acids research vol. 45,7 (2017): 3627-3633. doi:10.1093/nar/gkx182
+##Purpose: evaluation of databases documented in Wren et 2017  doi:10.1093/nar/gkx182
+##Package(s): tidyverse
+##Input file(s): wren_File003_all.csv, wren_File004_class.csv
+##Output file(s): wren_dbs.csv
 
 library(tidyverse)
 
-wren_all <- read_csv("wren_all.csv")
-wren_class <- read_csv("wren_class.csv")
+wren_all <- read_csv("wren_File003_all.csv")
+wren_class <- read_csv("wren_File004_class.csv") # Note: off_class = official classification
 wren_master <- full_join(wren_all, wren_class, by="PMID")
-## spot check via jounral names
+## spot check via journal names
 wren_master <- select(wren_master, 1, 3, 4, 6, 7, 2, 5)
 
 # some "duplication" e.g. PMIDs when multiple urls and multiple urls when diff journals, etc
 # checks
 
-journal_count <- wren_master %>% count(journal)
 class_count <- wren_master %>% count(off_class)
 
 ## remove no url and NA from wren_master = 27,350 to 27,301
 
-wren_master <- filter(wren_master, off_class != "NA" & off_class != "no_URL")
+wren_master <- filter(wren_master, off_class != "NA" & off_class != "no_URL") # correct
 wren_dbs <- filter(wren_master, off_class == "a_database")
-wren_dbs <- select(wren_dbs, 1, 4, 6, 3, 5, 7)
+wren_dbs <- select(wren_dbs, 1, 2, 4, 6, 3, 5, 7)
 
-db_journal_count <- wren_dbs %>% count(journal)
+write.csv(wren_dbs ,"wren_dbs.csv", row.names = FALSE) 
+
+##checks on confidence for NAR, Database-O, and non-NAR dbs - should be higher for first two
 
 wren_non_nar_dbs <- filter(wren_dbs, journal != "Nucleic Acids Res")
 wren_non_nar_dbs <- filter(wren_non_nar_dbs, confidence > 0.75)
--- wren_journals.R
+++ wren_journals.R
@@ -1,26 +1,25 @@
-# Starting with Wren's data from: 
-# Wren, Jonathan D et al. 
-# “Use it or lose it: citations predict the continued online availability of 
-# published bioinformatics resources.” 
-# Nucleic acids research vol. 45,7 (2017): 3627-3633. doi:10.1093/nar/gkx182
+##Purpose: evaluation of journals documented in Wren et 2017  doi:10.1093/nar/gkx182
+##Package(s): tidyverse
+##Input file(s): wren_File003_all.csv, wren_File004_class.csv
+##Output file(s): wren_journal_stats.csv
 
 library(tidyverse)
 
-wren_all <- read_csv("wren_all.csv")
-wren_class <- read_csv("wren_class.csv")
+wren_all <- read_csv("wren_File003_all.csv")
+wren_class <- read_csv("wren_File004_class.csv") # Note: off_class = official classification
 wren_master <- full_join(wren_all, wren_class, by="PMID")
-## spot check via jounral names
+## spot check via journal names
 wren_master <- select(wren_master, 1, 3, 4, 6, 7, 2, 5)
 
 # some "duplication" e.g. PMIDs when multiple urls and multiple urls when diff journals, etc
 # checks
 
-journal_count <- wren_master %>% count(journal)
-class_count <- wren_master %>% count(off_class)
-
 ## remove no url and NA from wren_master = 27,350 to 27,301
 
 wren_master <- filter(wren_master, off_class != "NA" & off_class != "no_URL")
+
+journal_count_all <- wren_master %>% count(journal)
+# 3096 
 
 journal_stats <- wren_master %>%
   group_by(journal) %>%
@@ -33,5 +32,3 @@ journal_stats <- select(journal_stats,2, 8:11)
 journal_stats <- distinct(journal_stats)
 
 write.csv(journal_stats,"wren_journal_stats.csv", row.names = FALSE) 
-
-top_20 <- filter(journal_stats, db > 20)
